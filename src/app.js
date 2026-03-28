935afcd6429e192d4721f052b9da99973e2634b1
Loicet <l.teta2@alustudent.com>
2026-03-28 04:54:16 -0400

    add Vercel URLs to CORS allowed origins

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const consentRoutes = require("./routes/consent");
 const adminRoutes = require("./routes/admin");
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:4173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:4173', 'https://ramba-med-tech.vercel.app', 'https://ramba-med-tech-mj8ozxfho-loice-tetas-projects.vercel.app'], credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
