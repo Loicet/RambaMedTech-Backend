699778f3358b37ec4314b39727147b864f462277
Loicet <l.teta2@alustudent.com>
2026-03-28 06:01:07 -0400

    downgrade to Prisma v5 for Render compatibility

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,8 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
 
-const connectionString = process.env.DATABASE_URL;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
